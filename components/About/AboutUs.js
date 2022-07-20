19a4e374b721ce4bdc9355008948a72f88fdbe31
medaVyshnavi <85098865+medaVyshnavi@users.noreply.github.com>
2022-07-20 12:31:28 +0530

    Update AboutUs.js

--- components/About/AboutUs.js
+++ components/About/AboutUs.js
@@ -12,7 +12,7 @@ const AboutUs = () => {
           <h2>About Us</h2>
           <p>
             Founded in 2010, we are a creative agency that produces lasting
-            results for our clients. We’ve partnered with many startups,
+            results for our clients. We have partnered with many startups,
             corporations, and nonprofits alike to craft designs that make real
             impact. We are always looking forward to creating brands, products,
             and digital experiences that connect with our clients' audiences.
